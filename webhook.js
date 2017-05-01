54370a1ca142fb4aaadd857a159504e260935bda
o qweoijqweojoiwq <o qweoijqweojoiwq>
2017-05-01 19:52:36 +0100

    Testing console.log for Get Started event

--- webhook.js
+++ webhook.js
@@ -28,7 +28,7 @@ app.post('/webhook', (req, res) => {
         if (event.message && event.message.text) {
           sendMessage(event, 1);
         } else if (event.postback && event.postback.payload === 'Started') {
-          sendMessage(event, 0);
+          console.log('Get started fired');
         }
       });
     });
