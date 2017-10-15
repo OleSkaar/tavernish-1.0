11e289d0955142e67a4b87d1d03237d6bcdab733
OleSkaar <olejohska@gmail.com>
2017-10-15 20:00:10 +0100

    Created new hmtl and js files to display the log.

---
+++ public/logs.js
@@ -0,0 +1,41 @@
+/*
+
+Display the 50 most recent log entries from log.json.
+
+*/
+
+// DATA CONTROLLER
+var dataController = (function() {
+    
+    var logfile = log
+    
+})();
+
+// UI CONTROLLER
+
+var UIController = (function() {
+    
+    var DOMstrings = {
+        log: 'log',
+    }
+    
+})();
+
+// GLOBAL CONTROLLER
+var controller = (function(dataCtrl, UICtrl) {
+       
+       return {
+        init: function() {
+            console.log('Log page loaded.');
+            
+            // 1. Load the names of all characters
+            
+            
+
+        }
+   }
+
+    
+})(dataController, UIController);
+
+
