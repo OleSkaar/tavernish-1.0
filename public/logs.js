b8fb89d8a32f3c63df1459a88e45668ec094c768
OleSkaar <olejohska@gmail.com>
2017-10-16 20:40:03 +0100

    Started to add XMLHTTPrequest to get JSON file.

--- public/logs.js
+++ public/logs.js
@@ -7,7 +7,22 @@ Display the 50 most recent log entries from log.json.
 // DATA CONTROLLER
 var dataController = (function() {
     
-    var logfile = log
+    return {
+        
+        getJSON: function() {
+            var url, req, res;
+            url = '/log.json'
+            req = new XMLHttpRequest();
+            req.open('GET', url);
+            req.responseType = 'json';
+            req.send();
+            req.onload = function() {
+                res = req.response    
+            }
+            return res
+        }
+        
+    }
     
 })();
 
@@ -28,8 +43,9 @@ var controller = (function(dataCtrl, UICtrl) {
         init: function() {
             console.log('Log page loaded.');
             
-            // 1. Load the names of all characters
-            
+            // 1. Load the JSON file
+            var file = dataCtrl.getJSON()
+            console.log(file);
             
 
         }
